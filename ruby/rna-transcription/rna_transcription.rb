50778d7704ed0bd9f14c21615f534860a4fd1227
Stephanie Nemeth <traumverloren@gmail.com>
2020-05-07 20:43:38 +0200

    Finish RNA transcription exercise

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,15 @@
+=begin
+Write your code for the 'Rna Transcription' exercise in this file. Make the tests in
+`rna_transcription_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/rna-transcription` directory.
+=end
+
+class Complement
+  NUCLEOTIDE_COMPLEMENT = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
+
+  def self.of_dna(strand)
+    strand.split('').map { |item| NUCLEOTIDE_COMPLEMENT[item] }.join
+  end
+end
