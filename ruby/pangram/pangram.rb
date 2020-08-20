7960e254bd585606305de79bed1b482850a4427f
Stephanie Nemeth <traumverloren@gmail.com>
2020-08-20 11:33:46 +0200

    change class to module

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,16 +1,10 @@
-=begin
-Write your code for the 'Pangram' exercise in this file. Make the tests in
-`pangram_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/pangram` directory.
-=end
-
-class Pangram
+module Pangram
   def self.pangram?(sentence)
     # Lowercase the sentence & put all the unique letters array and count them
     unique_letters =
-      sentence.downcase.split('').select { |letter| letter =~ /[a-z]/ }.uniq
+      sentence.downcase.each_char.select { |letter| letter =~ /[a-z]/ }.uniq
 
     unique_letters.length == 26
   end
