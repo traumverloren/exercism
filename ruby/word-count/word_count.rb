cf29d696f4f87dadb17e41304d049a0c9f92f7b1
Stephanie Nemeth <traumverloren@gmail.com>
2020-08-20 13:34:13 +0200

    move regex to constant

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,19 +1,14 @@
 # frozen_string_literal: true
 
-# Write your code for the 'Word Count' exercise in this file. Make the tests in
-# `word_count_test.rb` pass.
-#
-# To get started with TDD, see the `README.md` file in your
-# `ruby/word-count` directory.
-
 class Phrase
+  # regex for capturing words and apostrophes used in contractions or a number
+  REGEX = /\b\w+'?\w|\d+\b/.freeze
+
   def initialize(phrase)
     @phrase = phrase
   end
 
   def word_count
-    # regex for capturing words and apostrophes used in contractions or a number
-    regex = /\b\w+'?\w|\d+\b/
-    @phrase.downcase.scan(regex).tally
+    @phrase.downcase.scan(REGEX).tally
   end
 end
