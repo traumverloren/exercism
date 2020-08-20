b34eb6e33420e2d2b4d3111ec8bf7e0eb88386fc
Stephanie Nemeth <traumverloren@gmail.com>
2020-08-20 12:15:54 +0200

    reduce boilerplate using define_method

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,10 +1,4 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
+# frozen_string_literal: true
 
 class SpaceAge
   EARTH_ORBITAL_PERIOD_IN_SECONDS = 31_557_600
@@ -18,48 +12,15 @@ class SpaceAge
     saturn: 29.447498,
     uranus: 84.016846,
     neptune: 164.79132
-  }
+  }.freeze
 
   def initialize(seconds)
     @seconds = seconds
   end
 
-  def on_earth
-    on_planet(:earth)
-  end
-
-  def on_mercury
-    on_planet(:mercury)
-  end
-
-  def on_venus
-    on_planet(:venus)
-  end
-
-  def on_mars
-    on_planet(:mars)
-  end
-
-  def on_jupiter
-    on_planet(:jupiter)
-  end
-
-  def on_saturn
-    on_planet(:saturn)
-  end
-
-  def on_uranus
-    on_planet(:uranus)
-  end
-
-  def on_neptune
-    on_planet(:neptune)
-  end
-
-  private
-
-  def on_planet(planet)
-    @seconds.to_f /
-      (ORBITAL_PERIOD_RATES[planet] * EARTH_ORBITAL_PERIOD_IN_SECONDS)
+  %w[earth mercury venus mars jupiter saturn uranus neptune].each do |planet|
+    define_method "on_#{planet}" do
+      @seconds.fdiv(ORBITAL_PERIOD_RATES[planet.to_sym] * EARTH_ORBITAL_PERIOD_IN_SECONDS)
+    end
   end
 end
