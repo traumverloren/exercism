5fedfb14a7f0a799b80485863524693cfd9adc93
Stephanie Nemeth <traumverloren@gmail.com>
2020-09-10 16:17:11 +0200

    finish solution for triangle

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,8 +1,32 @@
-=begin
-Write your code for the 'Triangle' exercise in this file. Make the tests in
-`triangle_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/triangle` directory.
-=end
+class Triangle
+  attr_reader :sides
 
+  def initialize(sides)
+    @sides = sides
+  end
+
+  # all sides are equal lengths
+  def equilateral?
+    triangle? && @sides.uniq.count == 1
+  end
+
+  # at least two sides the same length
+  def isosceles?
+    triangle? && @sides.uniq.count <= 2
+  end
+
+  # all sides are different lengths
+  def scalene?
+    triangle? && @sides.uniq.count == 3
+  end
+
+  private
+
+  def triangle?
+    # all sides must be length > 0 and
+    # the sum of 2 sides must be greater than or equal to 3rd side
+    @sides.all? { |side| side.positive? && (@sides.sum - side) >= side }
+  end
+end
